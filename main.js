34a16a0ed467ab357f7372244ac93a64af5fccb9
Dimas Faidh Muzaki <dimasfaid@gmail.com>
2021-10-28 23:06:18 +0700

    first commit tinggal readmd

---
+++ main.js
@@ -0,0 +1,175 @@
+const movies = [{
+    image: "https://cps-static.rovicorp.com/1/adg/cov584/drv400/v406/v40682rf1u3.jpg",
+    title: "The Raid: Redemption",
+    year: "2011",
+    desc: "A S.W.A.T. team becomes trapped in a tenement run by a ruthless mobster and his army of killers and thugs."
+
+}, {
+    image: "https://thefilmstage.com/wp-content/uploads/2018/10/The-Night-Comes-For-Us.jpg",
+    title: "The Night Comes for Us",
+    year: "2018",
+    desc: "Ito, a gangland enforcer, caught amidst a treacherous and violent insurrection within his Triad crime family upon his return home from a stint abroad."
+
+}, {
+    image: "https://yf-alb-001-189081361.ap-south-1.elb.amazonaws.com/insights/wp-content/uploads/2021/10/a-world-without-movie-poster.jpg",
+    title: "A World Without",
+    year: "2021",
+    desc: "In a country where dating is outlawed, three teenage girls in search of happiness decide to join a reclusive community led by a charismatic leader."
+
+}, {
+    image: "http://barkerhost.com/wp-content/uploads/sites/4/2016/08/fVVU9hG6i6jeMv4HddeeTCAVzpF-0.jpg",
+    title: "The Raid 2",
+    year: "2014",
+    desc: "Only a short time after the first raid, Rama goes undercover with the thugs of Jakarta and plans to bring down the syndicate and uncover the corruption within his police force."
+
+}, {
+    image: "https://cdn0-production-images-kly.akamaized.net/cORq4ACDWIj_zmrhl4l_qhWjpoc=/35x0:1048x1350/640x853/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2945780/original/054513700_1571718788-perempuan_tanah_jahanam89.jpg",
+    title: "Impetigore",
+    year: "2019",
+    desc: "Maya with her best friend, Dini, tries to survive in a city without a family. She realized that she might inherit a property from her rich family. Maya returns to the village with Dini and unaware of the danger was waiting for her."
+
+}, {
+    image: "https://furyosa.com/wp-content/uploads/2019/10/Ratu-Ilmu-Hitam-poster-14-furyosa.jpg",
+    title: "The Queen of Black Magic",
+    year: "2019",
+    desc: "Families were terrorized at the orphanage. Someone wants them dead, apparently with black magic that is very deadly. She has a grudge and she was also born because of the sins of the orphans who formed her into the Queen of Black Magic."
+
+}, {
+    image: "https://media.senscritique.com/media/000019856376/source_big/Headshot.jpg",
+    title: "Headshot",
+    year: "2016",
+    desc: "Uwais plays a young man who washes ashore, an amnesiac with a serious head injury whose past comes back to haunt him shortly after being nursed back to health by a young doctor. Violence ensues. Sweet, sweet violence."
+
+}, {
+    image: "http://oltretomba.net/rev/img_b/pengabdi_setan_new_c1.jpg",
+    title: "Satan's Slaves",
+    year: "2017",
+    desc: "After dying from a strange illness that she suffered for 3 years, a mother returns home to pick up her children."
+
+}, {
+    image: "https://m.media-amazon.com/images/M/MV5BNWQxZjkzMDktZDBhMS00MDI5LTg2YzgtN2EyM2U0YTNkNWZmXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_.jpg",
+    title: "May the Devil Take You Too",
+    year: "2020",
+    desc: "Two years after escaping from demonic terror, the young woman is still haunted by unnatural vision. The dangers that await her and her friends are increasingly threatening: the figure of darkness rises to take their lives."
+
+}, {
+    image: "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/10/03/186071809.jpeg",
+    title: "Yuni",
+    year: "2021",
+    desc: "In her last year of secondary school, a bright Indonesian student is determined to pursue her education and resist getting married, despite the expectations of her community."
+
+}, {
+    image: "https://xinemas.com/data/upload/movies/2018/sebelum-iblis-menjemput.jpg",
+    title: "May the Devil Take You",
+    year: "2018",
+    desc: "When her estranged father falls into a mysterious coma, a young woman seeks answers at his old villa, where she and her stepsister uncover dark truths."
+
+}, {
+    image: "https://i1.wp.com/www.kaorinusantara.or.id/english/wp-content/uploads/2019/07/gundala.jpg?fit=813%2C1200&ssl=1",
+    title: "Gundala",
+    year: "2019",
+    desc: "Indonesia's preeminent comic book superhero and his alter ego Sancaka enter the cinematic universe to battle the wicked Pengkor and his diabolical squad of orphan assassins."
+
+}, {
+    image: "http://www.moviexclusive.com/Files/MB-Poster-A3.jpg",
+    title: "The 3rd Eye",
+    year: "2017",
+    desc: "When her little sister claims she sees the dead, Alia consults a psychic, who opens her own eyes to the vengeful ghosts haunting their childhood home."
+
+}, {
+    image: "https://i1.wp.com/perempuan.co/wp-content/uploads/2021/10/image001-21.jpg?fit=1024%2C1280&ssl=1",
+    title: "Nussa: The Movie",
+    year: "2021",
+    desc: "Nussa's life changed when he met a new friend who was much more liked by his friends than himself."
+
+}, {
+    image: "http://static.tvmaze.com/uploads/images/original_untouched/126/315555.jpg",
+    title: "Halfworlds",
+    year: "2015",
+    desc: "A parallel world of bloodthirsty creatures from South East Asian mythology has lived alongside humans for generations. They take on the appearance of humans themselves, and have been carefully concealed for centuries by a powerful family."
+
+}, {
+    image: "https://cloud.filmfed.com/movies/posters/l_249f7868-9b06-4afc-a8b5-29a964cdb39f.jpg",
+    title: "Lady Terminator",
+    year: "1989",
+    desc: "The spirit of an ancient evil queen posesses the body of a young anthropological student, who then goes on a murderous rampage."
+
+}, {
+    image: "https://media.fstatic.com/skLsIkwZ9SMDHV0nK5hVQeTFBjw=/290x478/smart/media/movies/covers/2020/07/MV5BMTU4ZDE1NjgtZWJlOS00N2VkLTk5MmMtMmI4ZDNkMGE4M2VlXkEyXkFqcGdeQX_mHkFreS.jpg",
+    title: "Pretty Little Liars",
+    year: "2019",
+    desc: "Indonesian teen drama based on the American TV series 'Pretty Little Liars', centering on the lives of four female students whose clique falls apart when their leader goes mysteriously missing."
+
+}, {
+    image: "https://i0.wp.com/dramacool.lc/wp-content/uploads/2021/10/ali-ratu-ratu-queens-2021.jpg",
+    title: "Ali & the Queens",
+    year: "2021",
+    desc: "After his father's passing, a teenager sets out for New York in search of his estranged mother and soon finds love and connection in unexpected places."
+
+}, {
+    image: "https://3.bp.blogspot.com/-iGT06_vXjL0/WwqyDOqkF6I/AAAAAAAAOq8/Vx_d5zA3P4EuAw1s7AHXkO9kGdhEYE2cACLcBGAs/s1600/SABRINA-POSTER.jpg",
+    title: "Sabrina",
+    year: "2018",
+    desc: "A toy manufacturer and his wife are terrorized by a demon after their orphaned niece tries to call her late mother's spirit."
+
+}, {
+    image: "http://4.bp.blogspot.com/_9xZXVskUTpk/TDxdpM1VJUI/AAAAAAAABLg/NVwdufrVdcg/s1600/merantau.jpg",
+    title: "Merantau",
+    year: "2009",
+    desc: "A young man from the countryside uses his skills in silat martial arts to survive the slave trade."
+
+}, ];
+
+
+
+const container = document.getElementById('movies-wrapper');
+
+movies.forEach((result, idx) => {
+
+    // Create card element
+    const card = document.createElement('div');
+    card.classList = 'card';
+
+    // Construct card content
+    const content = `
+    <div class="card" id="card-${idx}">
+        <a href="#" onclick="clickHandler(${idx})">
+            <img src=${result.image} alt="">
+            <div class="title">${result.title}</div>
+        </a>
+    </div>
+    `;
+
+    // Append newyly created card element to the container
+    container.innerHTML += content;
+})
+
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+let clickHandler = (idx) => {
+    modal.style.display = "block";
+    let movie = movies[idx];
+    document.getElementById("m_image").src = movie.image;
+    document.getElementById("m_desc").innerHTML = movie.desc;
+    document.getElementById("m_judul").innerHTML = movie.title + " (" + movie.year + ")";
+
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
